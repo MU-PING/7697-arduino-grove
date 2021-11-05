17d992b9cf1d6653d4764685b142b6b555df4aef
MU-PING <93152909+MU-PING@users.noreply.github.com>
2021-11-06 00:47:47 +0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -6,7 +6,7 @@ int i;
 // 以下函式設定 P5 為 Clock Pin ； P4 為 Data Pin ； Orientation 表示燈條顯示方向
 // 提醒：接線時必須符合程式的設定，不然無法運作 P5必須接在DCKI；P4必須接在DI
 
-Grove_LED_Bar _bar2(5, 4, 0);
+Grove_LED_Bar _bar2(5, 4, 0); //Clock Pin, Data Pin, Orientation
 
 void setup() {
   _bar2.begin();
